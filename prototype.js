848acead9a0fde0cf7edee74ed9f840f36673883
KratiGupta <kratigupta8545@gmail.com>
2021-09-23 12:02:34 +0530

    [Krati]Added : program for prototype

---
+++ prototype.js
@@ -0,0 +1,15 @@
+let userdetails ={
+    firstName: "Krati",
+    lastName: "Gupta",
+    phoneNo: "8707595662",
+    email:"kratigupta8545@gmail.com",
+    city: "Kanpur",
+    nationality : "Indian"
+}
+
+
+function test(){
+    console.log("hello there")
+}
+
+let testArray = ["krati","nishu","vanshika","akshara","tulika"];
